c789ea98e4c60ea612ec33845d7dd08c10b706c4
AllaPetrova <petrova.allapet@gmail.com>
2025-11-18 08:00:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from aiohttp import web
-import aiohttp_jinja2
-import jinja2
 from routes import setup_routes
-from models import init_db, close_db
 
 
-async def create_app():
-    app = web.Application()
-    app['config'] = {'database': 'sqlite:///ads.db'}
-    
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
-    
-    app.on_startup.append(init_db)
-    app.on_cleanup.append(close_db)
-    
-    setup_routes(app)
-    return app
-
+app = web.Application()
+setup_routes(app)
+app['ads_storage'] = {}
 
 if __name__ == '__main__':
-    app = create_app()
-    web.run_app(app, host='localhost', port=8080)
+    web.run_app(app, host='0.0.0.0', port=8080)
+
+
